1f59321d9a6e0d86fb688ee899cca715ad299e3e
dianka759 <dianka759@gmail.com>
2021-10-15 14:57:21 -0400

    added while loops

--- Loop_Challenges.js
+++ Loop_Challenges.js
@@ -1,20 +1,37 @@
 // Print odds 1-20
 // Using a loop write code that will console.log all of the odd values from 1 up to 20.
 
+// for loop
 for (var i = 1; i < 21; i++) {
     if (i % 2 != 0) {
         console.log(i)
     }
 }
 
+// while loop counting down
+var i = 21;
+while (i > 0, i--) {
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+}
 
 
 // Decreasing Multiples of 3
 // Using a loop write code that will console.log all of the values that are evenly divisible by 3 from 100 down to 0.
 
-for (var i = 100; i > 0; i--) {
+//for loop
+for (var i = 100; i >= 0; i--) {
     if (i % 3 == 0) {
         console.log(i)
+    }
+}
+
+//while loop
+var i = 100;
+while (i > 0, i--) {
+    if (i % 3 == 0) {
+        console.log(i);
     }
 }
 
@@ -22,28 +39,54 @@ for (var i = 100; i > 0; i--) {
 // Print the sequence
 // Using a loop write code that will console.log the values in this sequence 4, 2.5, 1, -0.5, -2, -3.5.
 
-var array = [4, 2.5, 1, -0.5, -2, -3.5]
-for (var i = 0; i < array.length; i++) {
-    console.log(array[i])
+// for loop
+for (var i = 4; i > -4; i -= 1.5) {
+    console.log(i)
+}
+
+// while loop
+var i = 5.5;
+while (i > -3.5) {
+    i -= 1.5;
+    console.log(i)
 }
 
 
 // Sigma
 // Write code that will add all of the values from 1-100 onto some variable sum and at the end console.log the result 1 + 2 + 3 + ... + 98 + 99 + 100. We should get back 5050 at the end.
 
+// for loop
 var sum = 0
 for (var i = 1; i < 101; i++) {
     sum += i
 }
 console.log(sum)
 
+// while loop
+var sum = 0
+var i = 0
+while (i < 101) {
+    sum += i;
+    i++
+}
+console.log(sum)
 
 
 // Factorial
 // Write code that will multiply all of the values from 1-12 onto some variable product and at the end console.log the result 1 * 2 * 3 * ... * 10 * 11 * 12. We should get back 479001600 at the end.
 
+// for loop
 var product = 1
 for (var i = 1; i < 13; i++) {
     product *= i
+}
+console.log(product)
+
+// while loop
+var product = 1;
+var i = 1;
+while (i < 13) {
+    product *= i;
+    i++
 }
 console.log(product)
